e4e9a4a11510ad0eb9ea7b020ce69b1e681cec15
anthony <aswingley@partsauthority.com>
2025-08-25 19:59:59 -0700

    1

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'mdds30_controller'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pyserial'],
     zip_safe=True,
     maintainer='Bryan Ribas',
     maintainer_email='bryanribas@gmail.com',
